cd2c6506ed894c632ef9b3c6720b7a2897b72ffc
Wendy <honeykatie@qq.com>
2015-04-14 00:52:37 -0400

    Key point!! - ng-app = "myApp"
    在app.js中，
    var myApp = angular.module('myApp', ['ngRoute', 'appControllers']);

--- builds/development/js/app.js
+++ builds/development/js/app.js
@@ -1,10 +1,12 @@
-var myApp = angular.module('myapp', ['ngRoute', 'appControllers']);
+var myApp = angular.module('myApp', ['ngRoute', 'appControllers']);
 // namespacing / app name / support module - link to controller
 // ngRoute来自于/angular-route.min.js
-var appControllers = angular.module('appControllers',[]);
 
-myApp.config(['$routeProvider', function($routeProvider){
-    $routeProvider.when('/login',{
-            templateUrl: 'views/login.html'
-        }); //$routeProvider when(path, route);
+var appControllers = angular.module('appControllers', []);
+
+myApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+    when('/login', {
+      templateUrl: 'views/login.html'
+    }); //$routeProvider when(path, route);
 }]);
