9b87c070aa2314c5ba28be1e948cb48a847822b4
Wendy <honeykatie@qq.com>
2015-04-14 00:42:47 -0400

    Ch2-3

---
+++ builds/development/js/app.js
@@ -0,0 +1,10 @@
+var myApp = angular.module('myapp', ['ngRoute', 'appControllers']);
+// namespacing / app name / support module - link to controller
+// ngRoute来自于/angular-route.min.js
+var appControllers = angular.module('appControllers',[]);
+
+myApp.config(['$routeProvider', function($routeProvider){
+    $routeProvider.when('/login',{
+            templateUrl: 'views/login.html'
+        }); //$routeProvider when(path, route);
+}]);
